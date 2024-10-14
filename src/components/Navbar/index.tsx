034b31181f817456f08377dff54dc4e7297d19b6
Gustavo Verdugo <42784327+GustavoVerdugo@users.noreply.github.com>
2024-10-14 18:25:21 -0300

    Actualizar index.tsx

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -16,18 +16,6 @@ const Navbar = (translations: any) => {
             className='h-18 w-auto'
             src='https://framerusercontent.com/images/yXEKb2ITjOWe8gU3149WkiqWAmA.png?scale-down-to=120' />
         </a>
-        <div className="lg:hidden">
-          <button className="navbar-burger flex items-center text-blue-600 p-3">
-            <svg
-              className="block h-4 w-4 fill-current"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <title>Mobile menu</title>
-              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-            </svg>
-          </button>
-        </div>
         <ul className="hidden absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 lg:flex lg:mx-auto lg:items-center lg:w-auto lg:space-x-6">
           <li>
             <a className="text-sm text-gray-800 hover:text-gray-900 font-semibold" href="#">
